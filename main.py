d05924c31e84396122bd13d3e42153ed429c4fb7
Whipander <mihaja2506@gmail.com>
2025-09-03 09:11:44 +0300

    feat: add POST /phones

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse, Response
-
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
 
 @app.get('/ping')
@@ -10,3 +11,28 @@ def ping():
 @app.get("/health")
 def get_health():
     return Response(content="Ok", status_code=200)
+
+class Characteristic:
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+phone_stored: List[Phone] = []
+
+def serialize_phone():
+    serialized_phone = []
+    for phone in phone_stored:
+        serialized_phone.append(phone.model_dump())
+
+    return serialized_phone
+@app.post("/phones")
+def add_phone(phone_to_add: List[Phone]):
+    for phone in phone_to_add:
+        phone_stored.append(phone)
+
+    return JSONResponse(content=serialize_phone(), status_code=201)
