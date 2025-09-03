b16b10b5eec9b304fc3435c361604a2563fdc777
Whipander <mihaja2506@gmail.com>
2025-09-03 09:04:06 +0300

    feat: add GET /health

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 
 app = FastAPI()
 
 @app.get('/ping')
 def ping():
     return JSONResponse({'message': 'pong'}, status_code=200)
+
+@app.get("/health")
+def get_health():
+    return Response(content="Ok", status_code=200)
